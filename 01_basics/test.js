e0b1cbd48538f695ee8786e454efca40a3c59682
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-25 12:22:06 +0000

    done with the container nodejs configurattion

---
+++ 01_basics/test.js
@@ -0,0 +1,14 @@
+console.log("Hitesh")
+//at first go to github 
+//then create a repo of myfirstjavascript
+//then go to code icon and select codespace which will open a vs code machine in the browser for you
+//now to add nodejs for that vscode go to commandpallatte and 
+//then search for container for the dev environment
+//now search for nodejs with javascript and select it this will set up node js for you
+//now start writing your codes
+
+
+//now to add the changed code in your github profile
+//you need to select source control icon (chimta wala) 
+//and then usme jaake jo bhi files krni h select kro aur commit krdo
+//ya phir push kr skte ho whi se navigate krke
