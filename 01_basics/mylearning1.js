327b41352e8b777434082373395c6f66a39a3c99
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-25 12:29:14 +0000

    mylearnings added here

---
+++ 01_basics/mylearning1.js
@@ -0,0 +1,8 @@
+//to understand javascript you need to know:
+
+//that all the code that I am writing here like(console.log("Ayush")) is just a text file but
+//it is the responsibility of interpreter or compiler to understand the correct syntax
+
+//dekho ye smjh aana chahiye ki javascript sirf ek language hai to isko execute krne ke liye mujhe ek software ya environment
+//ya interpreter ya compiler chahiye jise hum nodejs kehte hai (Ryan Dahl ki khoj) to koi bhi kaam krne se phle shree ganesh 
+//yani ki node js ki pooja anivarya hai
