9dd222e21fd51a8dbcd9d7b7eb7c2695b78668da
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-26 14:36:44 +0000

    conversion of datatypes to one another

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+console.log(typeof(score));
+
+//maan lo agr mujhe values number me chahiye to dusre var me store krke change krlo
+let valueinNumber= Number(score)
+console.log(typeof(valueinNumber))
+console.log(typeof(score))//ye change nhi hoga
+
+// for this you need to check if its NaN means not a Number
+// let say
+
+// let age=true
+// let age=undefined
+let age="45abc" //ab agr me isko number me convert kru to problem hai but 
+// mujhe ye bhi to nhi pta ki age ka type kya hai ky wo pure string hai ya number
+// to me ise number me convert krunga phir uske baad value ka type dekhunga
+
+let changeagetype= Number(age);
+console.log(typeof(changeagetype));
+console.log(changeagetype);//ye aapko aapka answer de dega
+// mtlb ye to convert hui hi nhi,
+// for null age it will give 0 
+// and for undefined in age will give NaN 
+// and for bool it give 1 or 0
