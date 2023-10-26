5334b5a976aaa5baaf9b8e3913056e7d56615a5f
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-26 14:15:10 +0000

    datatypes done!

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // it is used when we are writing code for newer version of js
+
+// #
+//alert(3+3) //it will not give us 6 coz browser ka js alag hota hai
+// aur node ka js engine ka alert alag hota hai
+
+//ab alert padhne ke liye google me jaake alert mdn search kr lena
+
+
+//# Datatypes:
+
+let name="Ayush" //string
+let age=23 //number 2 pow(53) then 
+let islogin=false //bool
+// bigint
+// string
+// boolean
+// null it is the standalone value its not a type, whn value is empty so its not undefined
+// undefined=> when value is not defined but initialized
+// symbol => unique values
+
+// object
+
+// # 
+console.log(typeof(name));
+console.log(typeof(undefined));
+console.log(typeof(null));//ye ek khami hai kyuki ye object return kr rha h
---
+++ 01_basics/varialbles.js
@@ -0,0 +1,21 @@
+const accountId=1 
+let accountmail="ayush@gmail.com"
+var accountpassword="45216"
+mycity="Chhindwara"
+
+
+accountmail="surya@gmail.com"
+accountpassword="12345"
+mycity="Noida"
+let mystate;
+
+console.log([accountmail,accountpassword,mycity,mystate])
+
+// Learnings
+// accountId=2  //we can not change const values
+
+/*we cannot use var most of the time because poore code me
+me jha jha accountpassword keyword use kiya jayega
+wha wha ye change ho jaega to usko anticipate krne
+ke liye let use krte hai jiska scope isi local file me 
+hoga aur khi nhi*/
