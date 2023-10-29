cd1fe3b9bbedbb45a67c36976bd4dd2d9c28351e
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-29 18:48:45 +0000

    types of datatype it will return whn do typeof( )

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,46 @@
+// Datatypes
+
+// Primitive
+// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+// Arrays, Objects, Functions
+
+const score=100
+const scoreValue=100.3
+
+const isloggedin=false
+const temperature=null
+console.log(typeof(temperature))//ye mujhe object type batayega
+let useremail;//its type is undefined now
+
+
+//the values in below consts are not same ye symbol ka jyadu hai
+const id = Symbol('123')
+const anotherid = Symbol('123')
+//these are not equal
+console.log(id==anotherid)
+
+// const bigNumber= 234561645615652n;//n will automatically convert const to BigInt
+// console.log(typeof(bigNumber))
+
+
+// Reference (Non Primitive)
+// Arrays, Objects, Functions
+
+//array
+const heros = ["shaktiman", "nagraj", "doga"]
+
+//object
+let myObj = {
+    name: "Ayush",
+    age: 23,
+}
+
+//functions
+const myfunc = function(){
+    console.log("Hello World");
+}
+myfunc();
+console.log(typeof(myfunc))//its type function but actually its function object
+
