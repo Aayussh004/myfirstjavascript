9fcf97108c606f2354d38b5b2f1a182429be4416
Ayush Suryawanshi <76591635+Aayussh004@users.noreply.github.com>
2023-10-29 20:26:47 +0000

    date and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,9 @@
+const myDate = new Date();
+console.log(myDate)//doesn't give good result and not readable at all
+console.log(myDate.toString())
+console.log(myDate.toDateString())//most useful ig
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())//this is also good
+console.log(myDate.toLocaleString())//this is also good
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleTimeString())//this one is good
