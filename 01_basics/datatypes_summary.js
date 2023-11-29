3bba3db6c7bc52a2117fc203537d6bbcc0b86557
Ayush Suryawanshi <ayushsuryawanshi004@gmail.com>
2023-11-29 20:53:57 +0530

    done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -6,46 +6,43 @@
 // Reference (Non Primitive)
 // Arrays, Objects, Functions
 
-
+//yex
 
 // ##########  Primitive
 // 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
 
-const score=100
-const scoreValue=100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isloggedin=false
-const temperature=null
-console.log(typeof(temperature))//ye mujhe object type batayega
-let useremail;//its type is undefined now
-
+const isloggedin = false;
+const temperature = null;
+console.log(typeof temperature); //ye mujhe object type batayega
+let useremail; //its type is undefined now
 
 //the values in below consts are not same ye symbol ka jyadu hai
-const id = Symbol('123')
-const anotherid = Symbol('123')
+const id = Symbol('123');
+const anotherid = Symbol('123');
 //these are not equal
-console.log(id==anotherid)
+console.log(id == anotherid);
 
 // const bigNumber= 234561645615652n;//n will automatically convert const to BigInt
 // console.log(typeof(bigNumber))
-
 
 //########### Reference (Non Primitive)
 // Arrays, Objects, Functions
 
 //array
-const heros = ["shaktiman", "nagraj", "doga"]
+const heros = ['shaktiman', 'nagraj', 'doga'];
 
 //object
 let myObj = {
-    name: "Ayush",
-    age: 23,
-}
+  name: 'Ayush',
+  age: 23,
+};
 
 //functions
-const myfunc = function(){
-    console.log("Hello World");
-}
+const myfunc = function () {
+  console.log('Hello World');
+};
 myfunc();
-console.log(typeof(myfunc))//its type function but actually its function object
-
+console.log(typeof myfunc); //its type function but actually its function object
